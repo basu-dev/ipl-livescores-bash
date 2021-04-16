46f29f5e9b2bef7839077f02357d057b6f057599
basu-dev <adbasudev54@gmail.com>
2021-04-16 23:57:02 +0545

    very difficult problem almost fixed now

--- ipl.sh
+++ ipl.sh
@@ -7,24 +7,37 @@ function clear(){
     pkill -RTMIN+10 i3blocks
 }
 
-raw=$(curl -s -4 "https://www.cricbuzz.com/")
-echo $raw | pup "li.videos-carousal-item" > ~/scripts/iplscore/test.html
-echo $raw | pup "li.videos-carousal-item" | \
- pup "a attr{href}"  |\
-  awk -F "/" '{print $4 " " $3}' > /tmp/livematches.txt 
-echo "STOP WATCHING" >> /tmp/livematches.txt
-match=$(cat /tmp/livematches.txt | rofi -dmenu -i -p "Select Match")
-$(rm /tmp/livematches.txt)
-echo $match
-if [[ $match == "STOP WATCHING" ]]
-then
-clear
-exit 0
-else
-matchId=$(echo $match | awk '{print $NF}')
-echo $matchId
-clear
-$(iplscore.sh $matchId)
+raw=$(curl -s -4 "https://www.cricbuzz.com/" | pup "li.videos-carousal-item")
+matchIds=()
+#echo $raw | pup "li.videos-carousal-item" > ~/scripts/iplscore/test.html
 
-fi
-pkill -f 'ipl';
+matchIds+=($(echo $raw | pup "a attr{href} "  | awk -F "/" '{print $3}'))
+
+matchTitles=$(echo "$raw" | pup "a attr{title}")
+
+title=$(echo "$matchTitles" | rofi -dmenu -i -p "Select Match")
+
+index=$(echo "$matchTitles" | grep -n "$title" | awk -F ":" '{print $1}')
+
+matchId=$(echo "${matchIds[$index]}")
+
+
+# echo "STOP WATCHING" >> /tmp/livematches.txt
+
+
+# match=$(cat /tmp/livematches.txt | rofi -dmenu -i -p "Select Match")
+
+# #$(rm /tmp/livematches.txt)
+# echo $match
+# if [[ $match == "STOP WATCHING" ]]
+# then
+# clear
+# exit 0
+# else
+# matchIds=$(echo $match | awk '{print $NF}')
+# echo $matchIds
+# clear
+# $(iplscore.sh $matchIds)
+
+# fi
+$(iplscore.sh $matchId)
